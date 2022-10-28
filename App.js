b61936e400c18e248b40a565cc86c152fe98c1d6
Minkee Yoon <ymg2610@icloud.com>
2022-10-28 14:06:46 -0600

    update

--- App.js
+++ App.js
@@ -1,28 +1,112 @@
-import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage } from 'react-native';
-import  Navigation from './components/Navigation';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OnboardingScreen from './screens/OnboardingScreen';
-import Home from './screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
-
-
-
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  AsyncStorage,
+  TextInput,
+  Button,
+} from "react-native";
+import Navigation from "./components/Navigation";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
 
 const AppStack = createNativeStackNavigator();
+const loggedInStates = {
+  NOT_LOGGED_IN: "NOT_LOGGED_IN",
+  LOGGED_IN: "LOGGED_IN",
+  CODE_SENT: "CODE_SENT",
+};
 
-const App = () =>{
+const App = () => {
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [loggedInState, setLoggedInStates] = React.useState(
+    loggedInStates.NOT_LOGGED_IN
+  );
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const {oneTimePassword, setOneTImePassword} = React.useState(null);
 
-   if (isFirstLaunch == true){
-return(
-  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
- 
-);
-  }else{
-    return <Navigation/>
+  if (isFirstLaunch == true) {
+    return <OnboardingScreen setFirstLaunch={setFirstLaunch} />;
+  } else if (loggedinState == loggedInStates.LOGGED_IN) {
+    return <Navigation />;
+  } else if (loggedInState == loggedInStates.NOT_LOGGED_IN) {
+    return (
+      <View>
+        <TextInput
+          style={StyleSheet.input}
+          placeholderTextColor="#4251f5"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        ></TextInput>
+        <Button
+          title="Send"
+          style={styles.button}
+          onPress={async () => {
+            console.log("A button was pressed!");
+            await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+              method: "POST",
+              headers: {
+                "content-type": "application/text",
+              },
+            });
+          }}
+        ></Button>
+      </View>
+    );
+  } else if (loggedInState == loggedInStates.CODE_SENT) {
+    return <View>
+      <TextInput
+          style={styles.input}
+          placeholderTextColor="#4251f5"
+          placeholder="One Time Password"
+          value={oneTimePassword}
+          onChangeText = {setOneTimePassword}
+          keyboardType = "numeric"
+    >
+    </TextInput>
+    <Button
+          title="Login"
+          style={styles.button}
+          onPress={async () => {
+            console.log("Login button was pressed!");
+            await fetch("https://dev.stedi.me/twofactorlogin", {
+              method: "POST",
+              headers: {
+                "content-type": "application/text",
+              },
+            }
+            )
+  />
+    </View>;
   }
-}
- export default App;
-
+};
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    marginTop: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  margin: {
+    marginTop: 100,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+});
